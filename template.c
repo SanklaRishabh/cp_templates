fc0bf416d51ca8d972fd10a1cceae54879fa4493
Rishabh Sankla <rishabhsankla1@gmail.com>
2024-01-01 17:54:42 +0530

    Created template.c

---
+++ template.c
@@ -0,0 +1,49 @@
+// Some commonly used headers.
+#include <limits.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Useful macros.
+// Comment if the header is unused.
+
+// Commons.
+#define inf 1e18
+#define ll long long
+#define tc int t
+#define ni int n
+#define til while
+#define to(X) for (int i = 0; i < X; i++)
+#define ret return
+
+// <stdio.h>
+#define pf printf
+#define sf scanf
+#define getTc tc;\
+    sf("%d", &t)
+#define getNi ni;\
+    sf("%d", &n); 
+
+// <stdlib.h>
+#define newChar(ptr, size) char *ptr = (char*)calloc(size, sizeof(char))
+#define newInt(ptr, size) int *ptr = (int*)calloc(size, sizeof(int))
+
+// <string.h>
+#define len strlen
+#define cmp strcmp
+#define cpy strcpy
+#define tok strtok
+#define cat strcat
+#define memS memset
+#define memM memmove
+#define memC memcpy
+
+int main(void) {
+    ret 0;
+}
