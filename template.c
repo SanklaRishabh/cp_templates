04fe5e4a44089251d9c35726b36dd2376415d10c
Rishabh Sankla <rishabhsankla1@gmail.com>
2024-02-10 16:27:33 +0530

    Added new definitions

--- template.c
+++ template.c
@@ -1,49 +1,51 @@
 // Some commonly used headers.
-#include <limits.h>
-#include <ctype.h>
-#include <math.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
+// #include <limits.h>
+// #include <ctype.h>
+// #include <math.h>
+// #include <stdarg.h>
+// #include <stdbool.h>
+// #include <stddef.h>
+// #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+// #include <string.h>
 
 // Useful macros.
 // Comment if the header is unused.
 
 // Commons.
+typedef long long ll;
+
 #define inf 1e18
-#define ll long long
 #define tc int t
 #define ni int n
-#define til while
-#define to(X) for (int i = 0; i < X; i++)
-#define ret return
+#define toi(X) for (int i = 0; i < X; i++)
+#define toi_ll(X) for (ll i = 0; i < X; i++)
+#define toj(X) for (int j = 0; j < X; j++)
+#define toj_ll(X) for (ll j = 0; j < X; j++)
+#define loopi(start, end) for (int i = start; i < end; i++)
+#define loopill(start, end) for (ll i = start; i < end; i++)
+#define loopj(start, end) for (int j = start; j < end; j++)
+#define loopjll(start, end) for (ll j = start; j < end; j++)
 
 // <stdio.h>
 #define pf printf
 #define sf scanf
 #define getTc tc;\
     sf("%d", &t)
-#define getNi ni;\
-    sf("%d", &n); 
+#define getN ni;\
+    sf("%d", &n) 
+#define set3(a, b, c) int a, b, c;\
+    sf("%d %d %d", &a, &b, &c)
+#define set2(x, y) int x, y;\
+    sf("%d %d", &x, &y)
 
 // <stdlib.h>
+#define newBool(ptr, size) bool *ptr = (bool*)calloc(size, sizeof(bool))
 #define newChar(ptr, size) char *ptr = (char*)calloc(size, sizeof(char))
 #define newInt(ptr, size) int *ptr = (int*)calloc(size, sizeof(int))
 
-// <string.h>
-#define len strlen
-#define cmp strcmp
-#define cpy strcpy
-#define tok strtok
-#define cat strcat
-#define memS memset
-#define memM memmove
-#define memC memcpy
-
 int main(void) {
-    ret 0;
+
+    return 0;
 }
