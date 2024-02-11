01722ef882ef1ca0b77212b6958a2d58e8ec2865
Rishabh Sankla <rishabhsankla1@gmail.com>
2024-02-11 21:36:46 +0530

    Added template.py

---
+++ template.py
@@ -0,0 +1,27 @@
+"""
+Comment the packages/modules that are not being used.
+"""
+from sys import stdin, stdout
+# from collections import defaultdict as dd, deque
+# from bisect import bisect_left as bl, bisect_right as br
+# from itertools import permutations, combinations
+# from heapq import heapify, heappop, heappush
+
+import sys
+# import math
+# import bisect
+# import string
+
+sys.setrecursionlimit(10 ** 8)
+
+read = lambda: int(input())
+strng = lambda: input().strip()
+map_int = lambda: map(int, strng().split())
+map_list = lambda: list(map_int())
+flush = lambda: stdout.flush()
+getline = lambda: stdin.readline()
+stdprint = lambda x: stdout.write(str(x))
+
+if __name__ == "__main__":
+    # Write code here.
+    pass
